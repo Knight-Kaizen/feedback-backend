23cecc759d31a02b00eb472d368322685b05ee93
Knight-Kaizen <auferstehung20@gmail.com>
2023-06-12 01:22:25 +0530

    added product routes

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,1 +1,111 @@
+const jwt = require('jsonwebtoken');
+const productDetailsCollection = require('../models/productModel');
 
+
+const verifyToken = async (tokenObj) => {
+    const token = tokenObj.slice(7);
+
+    if (!token) {
+        return ({
+            success: false,
+            message: 'token not received'
+        })
+    }
+    else {
+        try {
+            const result = await jwt.verify(token, process.env.TOKEN_KEY);
+            return ({
+                success: true,
+                message: 'User authorization success'
+            })
+        }
+        catch (err) {
+            console.log('checking error in token', err);
+            return ({
+                success: false,
+                message: 'user authorization failed, login again'
+            })
+        }
+    }
+}
+
+const addProduct = async (productDetails) => {
+    try {
+        // const token = productDetails.token.slice(7);
+
+        const result = await verifyToken(productDetails.token);
+        if(result.success){
+            const {product_name, logo_url, product_link, product_description, product_category} = productDetails;
+            const likes = 0;
+            const total_comments = 0;
+            const newProduct = new productDetailsCollection({
+                product_name, logo_url, product_link, product_description, product_category, likes, total_comments
+            })
+            const saveProduct = await newProduct.save();
+            // console.log(saveProduct);
+            return({
+                success: true,
+                message: 'product added successfully'
+            })
+        }
+        else{
+            console.log('Error in token verification');
+            return({
+                success: false,
+                message: result.message
+            })
+        }
+    }
+    catch (err) {
+        console.log('Error in add Product', err);
+        return ({
+            success: false,
+            message: 'Error in adding product, try again'
+        })
+    }
+}
+
+const getProducts = async(query, userSort)=>{
+    let sort = userSort;
+    if(!userSort){
+        sort = {_id:-1};
+    }
+    else if(sort == 'likes'){
+        sort = {likes: -1}
+    }
+    else if(sort == 'comments'){
+        sort = {total_comments: -1}
+    }
+    if (!query) {
+        const result = await productDetailsCollection.find().sort(sort);
+        return {
+            success: true,
+            data: result
+        };
+    }
+    else{
+        try{
+            const result = await productDetailsCollection.find({
+                'product_category': {
+                    $in: [query]
+                }
+            }).sort(sort);
+            return({
+                success: true,
+                data: result
+            })
+        }
+        catch(err){
+            console.log('error in getting products', err);
+            return({
+                success: false,
+                message: 'Could not fetch products, try again'
+            });
+        }
+    }
+}
+
+module.exports = {
+    addProduct,
+    getProducts
+}
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const connectDB = require('./configs/connectDB');
 const userController = require('./controllers/userController');
-
+const productController = require('./controllers/productController');
 
 const app = express();
 app.use(express.json());
@@ -12,11 +12,9 @@ connectDB();
 
 app.get('/', (req, res) => res.send('Backend working'));
 
-app.post('/register', async (req, res)=>{
-    // console.log();
-    const {name, email, mobile, password} = req.body;
+app.post('/user/register', async (req, res)=>{
     const {registerUser} = userController;
-    const result  = await registerUser({name, email, mobile, password});
+    const result  = await registerUser(req.body);
     if(result.success){
         res.send({
             success: true,
@@ -32,10 +30,8 @@ app.post('/register', async (req, res)=>{
 
 })
 
-app.post('/login', async(req, res)=>{
-    const {name, email} = req.body;
+app.post('/user/login', async(req, res)=>{
     const {loginUser} = userController;
-
     const result = await loginUser(req.body);
     if(result.success){
         res.send({
@@ -51,6 +47,63 @@ app.post('/login', async(req, res)=>{
     }
 })
 
+app.post('/product/add', async (req, res)=>{
+    let token = req.headers.authorization;
+    const {product_name, logo_url, product_link, product_description, product_category} = req.body;
+
+    const {addProduct} = productController;
+
+    const result = await addProduct({
+        token, product_name, logo_url, product_link, product_description, product_category
+    });
+    console.log(result);
+    if(result.success){
+        res.send({
+            success: true,
+            message: result.message
+        });
+    }
+    else{
+        res.send({
+            success: false,
+            message: result.message
+        });
+    }
+})
+
+app.get('/product/view', async(req, res)=>{
+    const {sort, product_category} = req.query;
+    const {getProducts} = productController;
+
+    const result = await getProducts(product_category, sort);
+    if(result.success){
+        res.send({
+            success: true,
+            data: result.data
+        });
+    }
+    else{
+        res.status(400).send({
+            success: false,
+            message: result.message
+        });
+    }
+})
+
+app.patch('/product/comment/:id', async(req, res)=>{
+    const productId = req.params.id;
+    res.send('hit');
+})
+
+app.patch('/product/like/:id', async(req, res)=>{
+    const productId = req.params.id;
+    res.send('hit');
+})
+
+app.patch('/product/edit/:id', async(req, res)=>{
+    const productId = req.params.id;
+    res.send('hit');
+})
 
 app.listen(port, () => {
     console.log('listening to port:', port);
