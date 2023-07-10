97e151aa66b989a45ddf4f5e806cec38173df04b
Knight-Kaizen <auferstehung20@gmail.com>
2023-07-10 18:49:48 +0530

    added express-validation to routes

--- routes/product.js
+++ routes/product.js
@@ -2,8 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 const productActions = require('../actions/product');
+const { validationResult, body } = require('express-validator');
 
-router.post('/product/add', async (req, res) => {
+const addValidation = [
+    body('product_name').isString().notEmpty().withMessage('Product name is required'),
+    body('logo_url').isString().notEmpty().withMessage('logo_url name is required'),
+    body('product_link').isString().notEmpty().withMessage('product_link name is required'),
+    body('product_description').isString().notEmpty().withMessage('product_description name is required'),
+    body('product_category').isString().notEmpty().withMessage('product_category name is required')
+];
+
+router.post('/product/add', addValidation, async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const error = errors.array()[0];
+        return res.status(400).json({ error: error.msg });
+    }
     let token = req.headers.authorization;
     const { product_name, logo_url, product_link, product_description, product_category } = req.body;
 
@@ -20,7 +34,7 @@ router.post('/product/add', async (req, res) => {
         });
     }
     else {
-        res.send({
+        res.status(400).json({
             success: false,
             message: result.message
         });
@@ -87,9 +101,9 @@ router.patch('/product/edit/:id', async (req, res) => {
     const productId = req.params.id;
     const token = req.headers.authorization;
     const { product_name, logo_url, product_link, product_description, product_category } = req.body;
-    const {editProductDetails} = productActions;
+    const { editProductDetails } = productActions;
 
-    const result = await editProductDetails({token, productId,product_name, logo_url, product_link, product_description, product_category });
+    const result = await editProductDetails({ token, productId, product_name, logo_url, product_link, product_description, product_category });
     if (result.success) {
         res.send({
             success: true,
--- routes/user.js
+++ routes/user.js
@@ -2,26 +2,55 @@ const express = require('express');
 const router = express.Router();
 
 const userActions = require('../actions/user');
+const { body, validationResult } = require('express-validator');
 
-router.post('/register', async (req, res) => {
-    const { registerUser } = userActions;
-    const result = await registerUser(req.body);
-    if (result.success) {
-        res.send({
-            success: true,
-            message: result.message
-        });
+const registerValidation = [
+    body('name').isString().notEmpty().withMessage('Name is required'),
+    body('password').isString().isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+    body('mobile').isString().isLength({ min: 10, max: 10 }).withMessage('Mobile must be a 10-digit string'),
+    body('email').isString().isEmail().withMessage('Email is invalid'),
+];
+
+router.post('/register',
+    registerValidation,
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            const error = errors.array()[0];
+            return res.status(400).json({ error: error.msg });
+        }
+        const { registerUser } = userActions;
+        const result = await registerUser(req.body);
+        if (result.success) {
+            res.send({
+                success: true,
+                message: result.message
+            });
+        }
+        else {
+            res.status(400).json({
+                success: false,
+                message: result.message
+            });
+        }
+
+    })
+
+const loginValidation = [
+    body('name').isString().notEmpty().withMessage('Name is required'),
+    body('password').isString().isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+];
+
+router.post('/login', 
+loginValidation, 
+async (req, res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const error = errors.array()[0];
+        return res.status(400).json({ error: error.msg });
     }
-    else {
-        res.send({
-            success: false,
-            message: result.message
-        });
-    }
 
-})
-
-router.post('/login', async (req, res) => {
     const { loginUser } = userActions;
     const result = await loginUser(req.body);
     if (result.success) {
@@ -31,7 +60,7 @@ router.post('/login', async (req, res) => {
         })
     }
     else {
-        res.send({
+        res.status(400).send({
             success: false,
             message: result.message
         })
